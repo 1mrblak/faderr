c34e07caf8833b67d3a2ede9e2f7aa0e835de8e7
Faderr J <johmfaderr@gmail.com>
2018-10-23 23:46:38 +0000

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+/*The picture slide*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+/*The scrolling*/
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/* mouseover resize image */
+function bigImg(x) {
+  x.style.height = "150px";
+  x.style.width = "150px";
+}
+
+function normalImg(x) {
+  x.style.height = "65px";
+  x.style.width = "65px";
+}
